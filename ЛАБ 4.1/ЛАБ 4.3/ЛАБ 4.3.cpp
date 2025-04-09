6938494de7137fb86760400fd2c01759ccdb3aee
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 19:00:05 +0300

    Add files via upload

---
+++ ЛАБ 4.1/ЛАБ 4.3/ЛАБ 4.3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x1, y1, x2, y2;
+    printf("Enter coordinates of the first point (x1 y1): ");
+    scanf_s("%f %f", &x1, &y1);
+    printf("Enter coordinates of the second point (x2 y2): ");
+    scanf_s("%f %f", &x2, &y2);
+    float distance = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    printf("Distance between points: %.2f\n", distance);
+    return 0;
+}
