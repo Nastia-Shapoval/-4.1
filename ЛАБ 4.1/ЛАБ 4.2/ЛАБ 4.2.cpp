9cff5f780c385d4647c7a26a54e412e316c65d89
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 18:59:33 +0300

    Add files via upload

---
+++ ЛАБ 4.1/ЛАБ 4.2/ЛАБ 4.2.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+    int birthYear, currentYear = 2025;
+    printf("Enter your birth year: ");
+    scanf_s("%d", &birthYear);
+    int age = currentYear - birthYear;
+    printf("Your age: %d years\n", age);
+    return 0;
+}
+
