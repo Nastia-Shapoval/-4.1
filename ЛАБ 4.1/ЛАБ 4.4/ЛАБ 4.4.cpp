13cad58c2d67bc066af553daa480a88c4c8fbee4
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 19:00:29 +0300

    Add files via upload

---
+++ ЛАБ 4.1/ЛАБ 4.4/ЛАБ 4.4.cpp
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+int main() {
+    float price, discount;
+    printf("Enter the product price: ");
+    scanf_s("%f", &price);
+    printf("Enter the discount percentage: ");
+    scanf_s("%f", &discount);
+    float finalPrice = price - (price * discount / 100);
+    printf("Final price: %.2f\n", finalPrice);
+    return 0;
+}
