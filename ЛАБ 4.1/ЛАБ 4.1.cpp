89b9a0b23117008a22c86c8ec2bb2d8bc0c09ff2
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-04-09 18:59:10 +0300

    Add files via upload

---
+++ ЛАБ 4.1/ЛАБ 4.1.cpp
@@ -0,0 +1,9 @@
+﻿#include <stdio.h>
+
+int main() {
+    int price = 450;
+    int discount = 15;
+    int finalPrice = price - (price * discount / 100);
+    printf("Price after discount: %d UAH\n", finalPrice);
+    return 0;
+}
